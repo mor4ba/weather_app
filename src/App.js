ef93398ba6ddb84aafb22da67a3dd1d1d93fdb3e
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-03-22 13:57:40 +0100

    change in app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useLocalStorageState from "use-local-storage-state";
+import Entrylist from "./components/entrylist/entrylist";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Form from "./components/form/form";
+import Weather from "./components/weather/weather";
+import { useState } from "react";
+import { useEffect } from "react";
+import { uid } from "uid";
 
-function App() {
+export default function App() {
+  const [entries, setEntries] = useLocalStorageState("entries", {
+    defaultValue: [],
+  });
+
+  const [weather, setWeather] = useState({
+    temperature: "10",
+    condition: "☀️",
+    isGoodWeather: true,
+  });
+
+  function handleAddEntry(newEntry) {
+    setEntries([{ id: uid(), ...newEntry }, ...entries]);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(fetchApi, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [weather]);
+
+  async function fetchApi() {
+    const response = await fetch("https://example-apis.vercel.app/api/weather");
+    const data = await response.json();
+    setWeather(data);
+  }
+
+  function removeEntry(id) {
+    setEntries(entries.filter((entry) => entry.id !== id));
+  }
+
+  const badWeatherEntries = entries.filter((entry) => !entry.isGoodWeather);
+  const goodWeatherEntries = entries.filter((entry) => entry.isGoodWeather);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Weather currentWeather={weather} />
+      <Form onAddEntry={handleAddEntry} />
+
+      <Entrylist
+        activities={
+          weather.isGoodWeather ? goodWeatherEntries : badWeatherEntries
+        }
+        onRemoveEntry={removeEntry}
+      />
+
+      <Footer />
+    </>
   );
 }
-
-export default App;
